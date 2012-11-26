4c03794965a097522939ce549a9778efad8312e3
James Reeves <jreeves@weavejester.com>
2012-11-26 21:57:48 +0000

    Added some tests

--- test/crypto/test/password.clj
+++ test/crypto/test/password.clj
@@ -1,7 +1,14 @@
-(ns crypto-password.core-test
-  (:use clojure.test
-        crypto-password.core))
+(ns crypto.test.password
+  (:use clojure.test)
+  (:require [crypto.password :as password]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-passwords
+  (are [s] (password/equal? s (password/encrypt s))
+    "a"
+    "foo"
+    "password"
+    "Testing"
+    "Test123"
+    "ÁäñßOÔ"
+    "großpösna"
+    "Some rather long pass phrase perhaps out of a book or poem"))
