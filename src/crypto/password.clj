56d11725382cdaa52e70e91c31ed7e0226926f04
James Reeves <jreeves@weavejester.com>
2012-11-26 20:47:06 +0000

    Initial commit

---
+++ src/crypto/password.clj
@@ -0,0 +1,6 @@
+(ns crypto-password.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/crypto/test/password.clj
@@ -0,0 +1,7 @@
+(ns crypto-password.core-test
+  (:use clojure.test
+        crypto-password.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
