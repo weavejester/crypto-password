218d8745a1929a40a7601448be73ce4250cba4b6
James Reeves <jreeves@weavejester.com>
2012-11-26 21:44:31 +0000

    Added equal? function to check password

--- src/crypto/password.clj
+++ src/crypto/password.clj
@@ -1,5 +1,6 @@
 (ns crypto.password
-  (:require [crypto.random :as random])
+  (:require [crypto.random :as random]
+            [clojure.string :as str])
   (:import javax.crypto.SecretKeyFactory
            javax.crypto.spec.PBEKeySpec
            org.apache.commons.codec.binary.Base64))
@@ -10,16 +11,34 @@
 (defn encode-int [i]
   (String. (Base64/encodeInteger (BigInteger/valueOf i))))
 
-(defn encrypt [s]
-  (let [salt        (random/bytes 8)
-        key-length  160
-        iterations  20000
-        key-spec    (PBEKeySpec. (.toCharArray s) salt iterations key-length)
-        key-factory (SecretKeyFactory/getInstance "PBKDF2WithHmacSHA1")]
-    (->> (.generateSecret key-factory key-spec)
-         (.getEncoded)
-         (base64)
-         (str (encode-str salt) "$")
-         (str (encode-int iterations) "$"))))
+(defn encrypt
+  ([raw]
+     (encrypt raw 20000))
+  ([raw iterations]
+     (encrypt raw iterations (random/bytes 8)))
+  ([raw iterations salt]
+     (let [key-length  160
+           key-spec    (PBEKeySpec. (.toCharArray raw) salt iterations key-length)
+           key-factory (SecretKeyFactory/getInstance "PBKDF2WithHmacSHA1")]
+       (->> (.generateSecret key-factory key-spec)
+            (.getEncoded)
+            (base64)
+            (str (encode-str salt) "$")
+            (str (encode-int iterations) "$")))))
 
+(defn decode-str [s]
+  (Base64/decodeBase64 (.getBytes s)))
 
+(defn decode-int [s]
+  (int (Base64/decodeInteger (.getBytes s))))
+
+(defn constant-time-eq? [^String a ^String b]
+  (if (and a b (= (.length a) (.length b)))
+    (zero? (reduce bit-or (map bit-xor (.getBytes a) (.getBytes b))))
+    false))
+
+(defn equal? [raw encrypted]
+  (let [[i s _]    (str/split encrypted #"\$")
+        salt       (decode-str s)
+        iterations (decode-int i)]
+    (constant-time-eq? encrypted (encrypt raw iterations salt))))
