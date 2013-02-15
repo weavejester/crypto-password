9c554596bd4c50903451b7050367e3a471e00166
James Reeves <jreeves@weavejester.com>
2013-02-15 16:02:55 +0000

    Released 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crypto-password "0.1.0-SNAPSHOT"
+(defproject crypto-password "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
