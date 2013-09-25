69f0a86e57b5b0cd2db45488e1fd680ce2b6aa26
James Reeves <jreeves@weavejester.com>
2013-09-25 14:17:10 +0100

    Updated Codox plugin version to 0.6.6

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/weavejester/crypto-password"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[codox "0.6.4"]]
+  :plugins [[codox "0.6.6"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [crypto-random "1.1.0"]
                  [crypto-equality "0.1.0"]
                  [commons-codec "1.6"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [com.lambdaworks/scrypt "1.3.3"]]
-  :codox {:src-dir-uri "http://github.com/weavejester/crypto-password/blob/0.1.0"
+  :codox {:src-dir-uri "http://github.com/weavejester/crypto-password/blob/0.1.0/"
           :src-linenum-anchor-prefix "L"}
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
