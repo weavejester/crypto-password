56e8577b2e1a9c537102d58ee6d188ac4c4a0575
James Reeves <jreeves@weavejester.com>
2016-02-20 18:30:32 +0000

    Update commons-codec dependency

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]
-                 [commons-codec "1.6"]
+                 [commons-codec "1.10"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [com.lambdaworks/scrypt "1.4.0"]]
   :codox {:src-dir-uri "http://github.com/weavejester/crypto-password/blob/0.1.3/"
