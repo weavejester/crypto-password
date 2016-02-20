90991808549af639d7bc8dd935b41358053e0247
James Reeves <jreeves@weavejester.com>
2016-02-20 16:05:54 +0000

    Update Clojure version and test profiles

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[codox "0.6.6"]]
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]
                  [commons-codec "1.6"]
@@ -12,8 +12,8 @@
                  [com.lambdaworks/scrypt "1.4.0"]]
   :codox {:src-dir-uri "http://github.com/weavejester/crypto-password/blob/0.1.3/"
           :src-linenum-anchor-prefix "L"}
+  :aliases {"test-all" ["with-profile" "+1.6:+1.7:+1.8" "test"]}
   :profiles
-  {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+  {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
