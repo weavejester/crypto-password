b444c826ab0a9015f70e4aa3afeff13c84bd3fbc
Johannes Lötzsch <git@johannesloetzsch.de>
2019-03-20 16:45:37 +0100

    Add test profiles for Clojure 1.8, 1.9 and 1.10

--- project.clj
+++ project.clj
@@ -15,8 +15,10 @@
    :project  {:name "Crypto-Password"}
    :metadata {:doc/format :markdown}
    :source-uri "http://github.com/weavejester/crypto-password/blob/{version}/{filepath}#L{line}"}
-  :aliases {"test-all" ["with-profile" "+1.6:+1.7:+1.8" "test"]}
+  :aliases {"test-all" ["with-profile" "+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
   :profiles
   {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
