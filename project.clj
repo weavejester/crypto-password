dab4ea55ee153f62a842a78e1739c83d72b31e9f
James Reeves <jreeves@weavejester.com>
2019-03-21 00:27:05 +0000

    Release 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crypto-password "0.2.0"
+(defproject crypto-password "0.2.1"
   :description "Library for securely hashing passwords"
   :url "https://github.com/weavejester/crypto-password"
   :license {:name "Eclipse Public License"
