7d55ee2447e3b7c89a8448295e23c57df0c120d7
meamuri <meammf@yandex.ru>
2020-12-13 11:48:15 +0300

    Update bcrypt dependency to 0.9.0

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]
                  [commons-codec "1.12"]
-                 [at.favre.lib/bcrypt "0.7.0"]
+                 [at.favre.lib/bcrypt "0.9.0"]
                  [com.lambdaworks/scrypt "1.4.0"]]
   :plugins [[lein-codox "0.9.4"]]
   :codox
