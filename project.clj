705a95f807a8e8f938e494ae9828e47959b20ff9
James Reeves <jreeves@weavejester.com>
2025-09-30 00:39:08 +0100

    Drop support for Clojure 1.5, 1.6, 1.7 and 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/weavejester/crypto-password"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [crypto-random "1.2.1"]
                  [crypto-equality "1.0.1"]
                  [commons-codec "1.19.0"]
@@ -16,10 +16,6 @@
    :project  {:name "Crypto-Password"}
    :metadata {:doc/format :markdown}
    :source-uri "http://github.com/weavejester/crypto-password/blob/{version}/{filepath}#L{line}"}
-  :aliases {"test-all" ["with-profile" "+1.6:+1.7:+1.8:+1.9:+1.10" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.10" "test"]}
   :profiles
-  {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
+  {:1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
